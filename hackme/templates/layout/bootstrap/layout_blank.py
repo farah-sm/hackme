3fe0d1480ac59b40c26d7fb2db164a5a72605b56
saed <65547726+farah-sm@users.noreply.github.com>
2024-09-08 15:30:28 +0100

    Update layout_blank.py

--- hackme/templates/layout/bootstrap/layout_blank.py
+++ hackme/templates/layout/bootstrap/layout_blank.py
@@ -6,9 +6,11 @@ This is an entry and Bootstrap class for the theme level.
 The init() function will be called in web_project/__init__.py
 """
 
-
+# This class, TemplateBootstrapLayoutBlank, is used to set up the context for the blank layout. 
+# It configures how pages using this layout should be structured.
 class TemplateBootstrapLayoutBlank:
     def init(context):
+        # Sets specific context variables such as "layout", "content_layout", and "display_customizer".
         context.update(
             {
                 "layout": "blank",
@@ -17,6 +19,8 @@ class TemplateBootstrapLayoutBlank:
             }
         )
         # map_context according to updated context values
+        #  Calls the map_context method from TemplateHelper to adjust 
+        # the context based on the current layout settings.
         TemplateHelper.map_context(context)
 
         return context
