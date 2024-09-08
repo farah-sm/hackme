bb41f406ca5cbe6a3fa37f4fe24ae4c134c52e79
saed <65547726+farah-sm@users.noreply.github.com>
2024-09-08 15:32:50 +0100

    Update layout_vertical.py

--- hackme/templates/layout/bootstrap/layout_vertical.py
+++ hackme/templates/layout/bootstrap/layout_vertical.py
@@ -8,8 +8,10 @@ The init() function will be called in web_project/__init__.py
 
 class TemplateBootstrapLayoutVertical:
     def init(context):
+        # All "True" indicates that the key should be present.
         context.update(
             {
+                
                 "layout": "vertical",
                 "content_navbar": True,
                 "content_layout": "compact",
@@ -20,8 +22,19 @@ class TemplateBootstrapLayoutVertical:
         )
 
         # map_context according to updated context values
+        # Calls the map_context method from TemplateHelper to adjust additional 
+        # context values based on the vertical layout settings.
         TemplateHelper.map_context(context)
 
 
 
         return context
+
+# This configuration makes it easy to apply a cohesive vertical layout across different pages, facilitating a uniform look and feel for the sections of your application that use this layout.
+
+
+
+
+
+
+
