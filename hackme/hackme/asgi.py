1cc01f2ef2b3e1313670931c76435073b6b84b71
saed <65547726+farah-sm@users.noreply.github.com>
2024-09-08 16:24:02 +0100

    Update asgi.py

--- hackme/hackme/asgi.py
+++ hackme/hackme/asgi.py
@@ -7,6 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
+# Configuration for ASGI (Asynchronous Server Gateway Interface), typically for handling asynchronous requests.
 import os
 
 from django.core.asgi import get_asgi_application
